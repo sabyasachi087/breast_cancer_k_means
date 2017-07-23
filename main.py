b52e2027ebec17a89a5f1e2c9b5c421017d6f2ed
Sabyasachi Roy Choudhury <sabyasachi087@gmail.com>
2017-07-23 11:56:21 +0530

    Phase 2 Completed

--- main.py
+++ main.py
@@ -13,17 +13,23 @@ srvc.curate(df)
 
 def phase1():    
     dp.plotHisto(df, comm.DATA_COLUMNS)
-    stats = comm.statistics(df)
+    stats = srvc.statistics(df)
     print(stats)
 
 def phase2():
-    print('Begin phase2')    
+    print('Begin phase2 \n')    
     ds = comm.getNumpyArray()
     s1, s2 = srvc.getRandomVectors(ds)   
-    classMap = srvc.classify(ds, s1, s2)
-    cluster1 = srvc.getClusterDataFrame(df, classMap['M1'])
-    cluster2 = srvc.getClusterDataFrame(df, classMap['M2'])
-
+    classMap = dict()
+    for i in range(0, 1500):
+        classMap = srvc.classify(ds, s1, s2)
+        s1 = classMap['MND1']
+        s2 = classMap['MND2']
+    print()
+    print('M2 : %s'%(s1))
+    print('M4 : %s'%(s2))
+    print()
+    print(srvc.generateReport(df, classMap))
 
 if __name__ == '__main__':
     phase2()
--- service.py
+++ service.py
@@ -3,6 +3,7 @@
 #
 import numpy as np
 import pandas as pd
+from _operator import index
 
 
 def curate(df, fill="mean"):
@@ -17,6 +18,22 @@ def curate(df, fill="mean"):
         df.fillna(df.mode(), inplace=True)
 
 
+def meanAndSd(s):
+    return s.describe()["mean"], s.describe()["std"]
+
+def statistics(df, DATA_COLUMNS):    
+    index = ['Mean', 'Median', 'StdDev']
+    stats = pd.DataFrame(np.random.randn(3, 9), columns=DATA_COLUMNS, index=index);
+    for column in DATA_COLUMNS:
+        s = df[column]
+        stats[column]['Mean'], stats[column]['StdDev'] = meanAndSd(s)
+        stats[column]['Median'] = stats[column].median()
+    return stats
+
+def getMeanVector(df, DATA_COLUMNS):
+    stat = statistics(df, DATA_COLUMNS)
+    return stat['Mean':'Mean'].as_matrix(DATA_COLUMNS)
+
 def getRandomVectors(ds):
     """
     The function takes a numpy array as argument and 
@@ -29,6 +46,7 @@ def getRandomVectors(ds):
         r2 = np.random.random_integers(0, n - 1)
     return ds[r1], ds[r2]
 
+
 def euclideanDistance(s1, s2):
     s = np.square(s1 - s2)
     return np.sqrt(np.sum(s))
@@ -39,22 +57,40 @@ def classify(ds, mean1, mean2):
     The function returns dictionary of key means and value as
     list of indexes belong to this category.Use keys as mean1 -> M1 and mean2 -> M2
     """
-    classMap = {'M1':list(), 'M2':list()}
+    meanNd1 = np.arange(ds.shape[1]);meanNd1.fill(0);meanNdCntr1 = 0
+    meanNd2 = np.arange(ds.shape[1]);meanNd2.fill(0);meanNdCntr2 = 0
+    indx_class_map = dict()
+    classMap = {'M1':list(), 'M2':list(), 'MND1':meanNd1, 'MND2':meanNd2, 'ID_CL_MAP':indx_class_map}    
     for indx in range(ds.shape[0]):
         d1 = euclideanDistance(mean1, ds[indx])
         d2 = euclideanDistance(mean2, ds[indx])
         if d1 < d2:
             classMap['M1'].extend([indx])
+            meanNd1 = ds[indx] + meanNd1
+            meanNdCntr1 = meanNdCntr1 + 1
+            indx_class_map[indx] = '2'
         elif d2 < d1:
             classMap['M2'].extend([indx])
+            meanNd2 = ds[indx] + meanNd2
+            meanNdCntr2 = meanNdCntr2 + 1
+            indx_class_map[indx] = '4'
         else:
             print('WARN :', 'Euclidean distance is same from means %s,%s \n from vector %s, assigning arbitrarily'\
                    % (mean1, mean2, ds[indx]))
             if np.random.random_integers(0, 1) == 1:
                 classMap['M1'].extend([indx])
+                meanNd1 = ds[indx] + meanNd1
+                meanNdCntr1 = meanNdCntr1 + 1
+                indx_class_map[indx] = '2'
             else:
                 classMap['M2'].extend([indx])
-    print('****************************END OF Classify*****************************\n')
+                meanNd2 = ds[indx] + meanNd2
+                meanNdCntr2 = meanNdCntr2 + 1
+                indx_class_map[indx] = '4'
+    # print('****************************END OF Classify*****************************\n')
+    classMap['MND1'] = meanNd1 / meanNdCntr1
+    classMap['MND2'] = meanNd2 / meanNdCntr2
+    classMap['ID_CL_MAP'] = indx_class_map
     return classMap
 
 def getClusterDataFrame(df, indexes):
@@ -62,6 +98,15 @@ def getClusterDataFrame(df, indexes):
     This function creates a new data frame with the rows specified by the indexes
     """
     cluster = df.copy(deep=True)
-    for indx in indexes:
-        cluster.drop(indx, inplace=True)
+    for indx in df.index.values:
+        if indx not in indexes:
+            cluster.drop(indx, inplace=True)
     return cluster
+
+def generateReport(df, classMap, limit=21):
+    report = pd.DataFrame(np.random.randn(limit, 3), columns=['ID', 'Class', 'Predicted_Class']);
+    for indx in range(0, limit):
+        report['ID'][indx] = df.iloc[indx]['Scn']
+        report['Class'][indx] = df.iloc[indx]['CLASS']
+        report['Predicted_Class'][indx] = classMap['ID_CL_MAP'][indx]
+    return report
